0f1958118ceacbfa89d80584757315dcbf5555a5
Douglas <mukhsinkadyrov@gmail.com>
2023-04-24 16:27:06 +0500

    adding testing workflow

--- src/components/PokemonsList/PokemonsList.jsx
+++ src/components/PokemonsList/PokemonsList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { PokemonTile } from "../../ui-kit/PokemonTile/PokemonTile";
-import axios from "axios";
 import "./PokemonsList.scss";
 import { LoadButton } from "../../ui-kit/Buttons/LoadButton";
 
